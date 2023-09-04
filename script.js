f019473f9aab3895eaa393da2be49be03c9c6841
Ahmed Youssef <ahmed7777youssef@gmail.com>
2023-09-04 18:22:48 +0300

    The last touch

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ const navContainer = document.querySelector(".nav-container");
 const navContent = document.querySelector(".nav-content-container");
 const navItems = document.querySelectorAll(".nav-items ul li");
 const contentContainer = document.querySelector(".content-container");
-const nameSearch = document.getElementById('name_search');
+const nameSearch = document.getElementById("name_search");
 
-const inputs_wrapper = document.getElementById('inputs_wrapper')
-const letterSearch = document.getElementById('letter_search');
+const inputs_wrapper = document.getElementById("inputs_wrapper");
+const letterSearch = document.getElementById("letter_search");
 const cardsWrapper = document.querySelector(
   ".content-container .cards-wrapper"
 );
-const spinner_container = document.querySelector('.spinner_container')
+const spinner_container = document.querySelector(".spinner_container");
+
+// form inputs
+let name_input,
+  email_input,
+  phone_input,
+  age_input,
+  password_input,
+  confirm_pass_input,
+  submit_button;
 
 let data;
 
@@ -19,7 +28,7 @@ const fetching_data = async (url) => {
   try {
     const response = await fetch(url);
     data = await response.json();
-    console.log(data)
+    console.log(data);
   } catch (error) {
     console.log(error);
   }
@@ -71,8 +80,7 @@ const get_tags = (meal) => {
 };
 
 const go_to_meal_details = async (mealId) => {
-
-  spinner_container.style.display = 'flex';
+  spinner_container.style.display = "flex";
   let url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
   await fetching_data(url);
 
@@ -114,13 +122,12 @@ const go_to_meal_details = async (mealId) => {
 
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
+  spinner_container.style.display = "none";
 };
 
 //======================== fetch food data from api to put it in the home page ===================
 const home_page = async () => {
-
-  spinner_container.style.display = 'flex';
+  spinner_container.style.display = "flex";
 
   const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
   await fetching_data(url);
@@ -137,23 +144,23 @@ const home_page = async () => {
 
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
+  spinner_container.style.display = "none";
 };
 
 home_page();
 
 // ============================== Search ===============================
 
-let searchQuery = '' ;
+let searchQuery = "";
 const handleChange = async (event) => {
-  spinner_container.style.display = 'flex';
+  spinner_container.style.display = "flex";
   searchQuery = event.target.value;
-  let url ;
-  console.log(event.target.id)
-  if(event.target.id == "name_search"){
-    url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`
-  }else{
-    url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchQuery}`
+  let url;
+  console.log(event.target.id);
+  if (event.target.id == "name_search") {
+    url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchQuery}`;
+  } else {
+    url = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchQuery}`;
   }
   await fetching_data(url);
 
@@ -169,22 +176,22 @@ const handleChange = async (event) => {
 
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
-}
+  spinner_container.style.display = "none";
+};
 
 const go_to_search = async () => {
-  spinner_container.style.display = 'flex';
-  inputs_wrapper.style.display = 'flex';
+  spinner_container.style.display = "flex";
+  inputs_wrapper.style.display = "flex";
   cardsWrapper.innerHTML = " ";
-  handleMenubar()
-  spinner_container.style.display = 'none';
-}
+  handleMenubar();
+  spinner_container.style.display = "none";
+};
 
 // ============================= categories ============================
 const go_to_categories = async () => {
-  spinner_container.style.display = 'flex';
-  inputs_wrapper.style.display = 'none';
-  handleMenubar()
+  spinner_container.style.display = "flex";
+  inputs_wrapper.style.display = "none";
+  handleMenubar();
 
   let url = "https://www.themealdb.com/api/json/v1/1/categories.php";
   await fetching_data(url);
@@ -201,13 +208,13 @@ const go_to_categories = async () => {
       "</div>";
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
+  spinner_container.style.display = "none";
 };
 
 // =========== filter by category ============
 
 const filter_by_category = async (category) => {
-  spinner_container.style.display = 'flex';
+  spinner_container.style.display = "flex";
   let url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
   await fetching_data(url);
 
@@ -223,14 +230,14 @@ const filter_by_category = async (category) => {
 
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
+  spinner_container.style.display = "none";
 };
 
 // ============================= Areas ============================
 
 const go_to_areas = async () => {
-  spinner_container.style.display = 'flex';
-  inputs_wrapper.style.display = 'none';
+  spinner_container.style.display = "flex";
+  inputs_wrapper.style.display = "none";
   handleMenubar();
 
   let url = "https://www.themealdb.com/api/json/v1/1/list.php?a=list";
@@ -245,11 +252,11 @@ const go_to_areas = async () => {
       "</div>";
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
+  spinner_container.style.display = "none";
 };
 // =========== filter by area ============
 const filter_by_area = async (area) => {
-  spinner_container.style.display = 'flex';
+  spinner_container.style.display = "flex";
 
   let url = `https://www.themealdb.com/api/json/v1/1/filter.php?a=${area}`;
   await fetching_data(url);
@@ -266,15 +273,15 @@ const filter_by_area = async (area) => {
 
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
+  spinner_container.style.display = "none";
 };
 
 // ============================= Ingredients ============================
 
 const go_to_ingredients = async () => {
-  spinner_container.style.display = 'flex';
+  spinner_container.style.display = "flex";
 
-  inputs_wrapper.style.display = 'none';
+  inputs_wrapper.style.display = "none";
   handleMenubar();
 
   let url = "https://www.themealdb.com/api/json/v1/1/list.php?i=list";
@@ -291,13 +298,13 @@ const go_to_ingredients = async () => {
 
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
+  spinner_container.style.display = "none";
 };
 
 // =========== filter by ingredient ============
 
 const filter_by_ingredient = async (ingredient) => {
-  spinner_container.style.display = 'flex';
+  spinner_container.style.display = "flex";
 
   let url = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`;
   await fetching_data(url);
@@ -314,5 +321,109 @@ const filter_by_ingredient = async (ingredient) => {
 
     cardsWrapper.innerHTML += htmlCode;
   });
-  spinner_container.style.display = 'none';
+  spinner_container.style.display = "none";
+};
+
+// ============================= Ingredients ============================
+const go_to_contact = () => {
+  console.log("hellooooo");
+
+  const htmlCode =
+    "<form>" +
+    '<div class="inputs_row">' +
+    '<div>'+
+    '<input id="name_input" type="text" placeholder="Enter Your Name" oninput="handle_form_inputs(event)" onchange="handle_form_inputs(event)">' +
+    '<div class="error_message">Name Not Valide</div>' +
+    '</div>'+
+    '<div>'+
+    '<input id="email_input" type="email" placeholder="Enter Your Email" oninput="handle_form_inputs(event)" onchange="handle_form_inputs(event)">' +
+    '<div class="error_message">Email Not Valide</div>' +
+    '</div>'+
+    "</div>" +
+    '<div class="inputs_row">' +
+    '<div>'+
+    '<input id="phone_input" type="tel"  placeholder="Enter Your Phone" oninput="handle_form_inputs(event)" onchange="handle_form_inputs(event)">' +
+    '<div class="error_message">Phone Not Valide</div>' +
+    '</div>'+
+    '<div>'+
+    '<input id="age_input" type="number" placeholder="Enter Your Age" oninput="handle_form_inputs(event)" onchange="handle_form_inputs(event)">' +
+    '<div class="error_message">Must be greater than 11</div>' +
+    '</div>'+
+    "</div>" +
+    '<div class="inputs_row">' +
+    '<div>'+
+    '<input id="password_input" type="password"  placeholder="Enter Your password" oninput="handle_form_inputs(event)" onchange="handle_form_inputs(event)">' +
+    '<div class="error_message">password Not Valide</div>' +
+    '</div>'+
+    '<div>'+
+    '<input id="confirm_pass_input" type="password" placeholder="Repassword" oninput="handle_form_inputs(event)" onchange="handle_form_inputs(event)">' +
+    '<div class="error_message">Not Correct</div>' +
+    '</div>'+
+    "</div>" +
+    '<button id="submit" type="submit" disabled>Submit</button>' +
+    "</from>";
+
+  cardsWrapper.innerHTML = htmlCode;
+
+  // form inputs
+  name_input = document.getElementById("name_input");
+  email_input = document.getElementById("email_input");
+  phone_input = document.getElementById("phone_input");
+  age_input = document.getElementById("age_input");
+  password_input = document.getElementById("password_input");
+  confirm_pass_input = document.getElementById("confirm_pass_input");
+  submit_button = document.getElementById("submit");
+  error_messages = document.querySelectorAll('.error_message')
+};
+
+const validateForm = () => {
+  const password_regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/ ;
+  const gmail_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ ;
+
+  if(name_input.value.trim() != ""){
+    error_messages[0].style.display = 'none'
+  }else{
+    error_messages[0].style.display = 'block'
+  }
+
+  if(gmail_regex.test(email_input.value.trim())){
+    error_messages[1].style.display = 'none'
+  }else{
+    error_messages[1].style.display = 'block'
+  }
+
+  if(phone_input.value.trim().length == 11){
+    error_messages[2].style.display = 'none'
+  }else{
+    error_messages[2].style.display = 'block'
+  }
+
+  if(age_input.value.trim() > 11){
+    error_messages[3].style.display = 'none'
+  }else{
+    error_messages[3].style.display = 'block'
+  }
+
+  if(password_regex.test(password_input.value.trim())){
+    error_messages[4].style.display = 'none'
+  }else{
+    error_messages[4].style.display = 'block'
+  }
+
+  if (
+    name_input.value.trim() != "" &&
+    email_input.value.trim() != "" &&
+    phone_input.value.trim() != "" &&
+    age_input.value.trim() > 0 &&
+    password_regex.test(password_input.value.trim()) &&
+    confirm_pass_input.value.trim() == password_input.value.trim()
+  ) {
+    submit_button.disabled = false;
+  }else{
+    submit_button.disabled = true;
+  }
+};
+
+const handle_form_inputs = (event) => {
+  validateForm();
 };
